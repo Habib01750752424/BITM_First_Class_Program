11573642b4b2e63b29e4ad277014996ddfe77183
Md. Habibur Rahman <habibcsepust@gmail.com>
2019-09-09 16:27:53 +0600

    Added BITM first program in github

---
+++ BitmFirstProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int age;
+            string contactNo = "";
+            string address = "";
+            Console.Write("Enter Your Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Your Age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Your ContactNumber: ");
+            contactNo = Console.ReadLine();
+
+            Console.Write("Enter Your Address: ");
+            address = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("My name is: " + name);
+            Console.WriteLine("My age: " + age);
+            Console.WriteLine("My contact number: " + contactNo);
+            Console.WriteLine("and\nMy Address is: " + address);
+
+            Console.ReadKey();
+        }
+    }
+}
